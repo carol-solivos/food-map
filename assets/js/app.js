e651462522e2ffeef020cc35b58257a4ff2c6ab1
carol-solivos <carol.solivos@gmail.com>
2017-12-14 01:26:35 -0400

    mapa dataset, modal y hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,14 +3,14 @@ var delay = 2000;
 setTimeout(function(){ window.location = 'buscar.html'; }, delay);*/
 
 /*splash carga misma página*/
+
 $(document).ready(function(){
   $(function(){
     setTimeout(function() {
         $('.splash').fadeOut('3000');
     }, 3500); 
   });
-
-  function todos(){
+  $(function todos(){
     $('#images').text('');
     var cafes1;
     var comidaRapida1;
@@ -19,44 +19,62 @@ $(document).ready(function(){
       cafes1 = data.cafes[i];
       comidaRapida1 = data.comidaRapida[i];
       restaurantes1 = data.restaurantes[i];
-      $('#images').append('<img src="assets/images/' + cafes1 + '">');
-      $('#images').append('<img src="assets/images/' + comidaRapida1 + '">');
-      $('#images').append('<img src="assets/images/' + restaurantes1 + '">');
+      $('#images').append('<img class="imgs" data-type="restaurant" src="assets/images/' + cafes1 + '">');
+      $('#images').append('<img class="imgs" data-type="restaurant" src="assets/images/' + comidaRapida1 + '">');
+      $('#images').append('<img class="imgs" data-type="restaurant" src="assets/images/' + restaurantes1 + '">');
     }
-};
-  todos();
-  $('#todos').click(todos);
+    $('#todos').click(todos);
+    hover();
+    click();
+  });  
   $('#cafe').click(function(){
     $('#images').text('');
     var cafes;
     for (var i = 0; i < data.cafes.length; i++) {
       cafes = data.cafes[i];
-      $('#images').append('<img src="assets/images/' + cafes + '">');
+      $('#images').append('<img class="imgs" data-type="cafe" src="assets/images/' + cafes + '">');
     }
-    
+    hover();
+    click();
   });
   $('#restaurante').click(function(){
     $('#images').text('');
     var restaurante;
     for (var i = 0; i < data.restaurantes.length; i++) {
       restaurante = data.restaurantes[i];
-      $('#images').append('<img src="assets/images/' + restaurante + '">');
+      $('#images').append('<img class="imgs" data-type="restaurant" src="assets/images/' + restaurante + '">');
     }
-    
+    hover();
+    click();
   });
   $('#rapida').click(function(){
     $('#images').text('');
     var comidaRapida;
     for (var i = 0; i < data.comidaRapida.length; i++) {
       comidaRapida = data.comidaRapida[i];
-      $('#images').append('<img src="assets/images/' + comidaRapida + '">');
+      $('#images').append('<img class="imgs" data-type="bakery" src="assets/images/' + comidaRapida + '">');
     }
-    
+    hover();
+    click();
   });
+  function hover(){
+    $("#images img").hover(function() {
+      $(this).css("opacity","0.5");
+        }, function() {
+      $(this).css("opacity","1");
+    });
+  };
+  function click(){
+    $("#images img").click(function() {
+      $(this).attr("data-toggle","modal");
+      $(this).attr("data-target","#myModal");
+    })
+  };
 });
 
 var map;
 var infowindow;
+var typeMap = document.querySelector('.imgs').dataset;
 
 function initMap() {
         var pyrmont = {lat: -33.419, lng: -70.643};
@@ -71,7 +89,7 @@ function initMap() {
         service.nearbySearch({
           location: pyrmont,
           radius: 500,
-          type: ['restaurant'] //cafe bakery
+          type: [typeMap] //cafe bakery restaurant
         }, callback);
 }
 
