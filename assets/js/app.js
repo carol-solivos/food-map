115fcee562ddb28f9cd026312dc40aff5e74ed9b
carol-solivos <carol.solivos@gmail.com>
2017-12-13 20:42:28 -0400

    filtro y dropdown

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,75 +4,95 @@ setTimeout(function(){ window.location = 'buscar.html'; }, delay);*/
 
 /*splash carga misma página*/
 $(document).ready(function(){
-    $(function(){
+  $(function(){
     setTimeout(function() {
         $('.splash').fadeOut('3000');
     }, 3500); 
+  });
 
-    });
+  function todos(){
+    $('#images').text('');
+    var cafes1;
+    var comidaRapida1;
+    var restaurantes1;
+    for (var i = 0; i < data.cafes.length; i++) {
+      cafes1 = data.cafes[i];
+      comidaRapida1 = data.comidaRapida[i];
+      restaurantes1 = data.restaurantes[i];
+      $('#images').append('<img src="assets/images/' + cafes1 + '">');
+      $('#images').append('<img src="assets/images/' + comidaRapida1 + '">');
+      $('#images').append('<img src="assets/images/' + restaurantes1 + '">');
+    }
+};
+  todos();
+  $('#todos').click(todos);
+  $('#cafe').click(function(){
+    $('#images').text('');
+    var cafes;
+    for (var i = 0; i < data.cafes.length; i++) {
+      cafes = data.cafes[i];
+      $('#images').append('<img src="assets/images/' + cafes + '">');
+    }
+    
+  });
+  $('#restaurante').click(function(){
+    $('#images').text('');
+    var restaurante;
+    for (var i = 0; i < data.restaurantes.length; i++) {
+      restaurante = data.restaurantes[i];
+      $('#images').append('<img src="assets/images/' + restaurante + '">');
+    }
+    
+  });
+  $('#rapida').click(function(){
+    $('#images').text('');
+    var comidaRapida;
+    for (var i = 0; i < data.comidaRapida.length; i++) {
+      comidaRapida = data.comidaRapida[i];
+      $('#images').append('<img src="assets/images/' + comidaRapida + '">');
+    }
+    
+  });
 });
 
 var map;
 var infowindow;
 
 function initMap() {
-    var pyrmont = {lat: -33.867, lng: 151.195};
+        var pyrmont = {lat: -33.419, lng: -70.643};
 
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 12
-  });
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: pyrmont,
+          zoom: 15
+        });
 
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch({
-      location: pyrmont,
-      radius: 500,
-      type: ['restaurantes']
-  }, callback);
-    if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+        infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+        service.nearbySearch({
+          location: pyrmont,
+          radius: 500,
+          type: ['restaurant'] //cafe bakery
+        }, callback);
+}
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+          }
         }
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
-
-function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-    }
-}
-}
-
 function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location
-  });
+        var placeLoc = place.geometry.location;
+        var marker = new google.maps.Marker({
+          map: map,
+          position: place.geometry.location
+        });
 
-    google.maps.event.addListener(marker, 'click', function() {
-      infowindow.setContent(place.name);
-      infowindow.open(map, this);
-  });
-}
+        google.maps.event.addListener(marker, 'click', function() {
+          infowindow.setContent(place.name);
+          infowindow.open(map, this);
+        });
+};
+
---
+++ assets/js/data.js
@@ -0,0 +1,5 @@
+var data = {
+	'cafes': ['coffee-1.jpg', 'coffee-shops.jpg', 'DSC_0002.jpg', 'frenchcoffeeshop3.jpg', 'starbucks.jpg'],
+	'restaurantes': ['r8_n.jpg', 'r11onlinereservationpark.jpg', 'restaurantinterior1__1.jpg', 'rGBLW_exterior1.jpg', 'restaurant.png'],
+	'comidaRapida': ['TelepizzaGranViaBarcelona.jpg', 'mcdonalds-meal_0-1cgqmtg.jpg', 'fast-food-fact.jpg', 'feature-image-kids-fast-food.jpg', '673x6731453494563_Subway.jpg']
+}
